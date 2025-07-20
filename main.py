f6d04809b8902d0ce679f53aab1ca6af2ed29635
dailingx <468551414@qq.com>
2025-07-20 17:39:35 +0800

    init

--- main.py
+++ main.py
@@ -24,6 +24,8 @@ cosyvoice = CosyVoice2('pretrained_models/CosyVoice2-0.5B', load_jit=True, load_
 async def vllm_tts(request: Request):
     # 获取原始请求的 JSON 数据
     body = await request.body()
+    data = await request.json()
+    logging.info(f"request: {body}, {data}")
     tts_text = body.text
     prompt_speech_16k = load_wav('./asset/zero_shot_prompt.wav', 16000)
     set_all_random_seed(123)
