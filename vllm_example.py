c73d12417e070962888d0ee387c7831263b79039
dailingx <468551414@qq.com>
2025-07-20 14:03:30 +0800

    init

--- vllm_example.py
+++ vllm_example.py
@@ -12,11 +12,17 @@ from tqdm import tqdm
 
 def main():
     cosyvoice = CosyVoice2('pretrained_models/CosyVoice2-0.5B', load_jit=True, load_trt=True, load_vllm=True, fp16=True)
+    print("load CosyVoice2 success.")
     prompt_speech_16k = load_wav('./asset/zero_shot_prompt.wav', 16000)
-    for i in tqdm(range(100)):
-        set_all_random_seed(i)
-        for _, _ in enumerate(cosyvoice.inference_zero_shot('收到好友从远方寄来的生日礼物，那份意外的惊喜与深深的祝福让我心中充满了甜蜜的快乐，笑容如花儿般绽放。', '希望你以后能够做的比我还好呦。', prompt_speech_16k, stream=False)):
-            continue
+    # for i in tqdm(range(100)):
+    #     set_all_random_seed(i)
+    #     for _, _ in enumerate(cosyvoice.inference_zero_shot('收到好友从远方寄来的生日礼物，那份意外的惊喜与深深的祝福让我心中充满了甜蜜的快乐，笑容如花儿般绽放。', '希望你以后能够做的比我还好呦。', prompt_speech_16k, stream=False)):
+    #         continue
+    set_all_random_seed(123)
+    for _, _ in enumerate(cosyvoice.inference_zero_shot(
+            '收到好友从远方寄来的生日礼物，那份意外的惊喜与深深的祝福让我心中充满了甜蜜的快乐，笑容如花儿般绽放。',
+            '希望你以后能够做的比我还好呦。', prompt_speech_16k, stream=False)):
+        continue
 
 
 if __name__ == '__main__':
