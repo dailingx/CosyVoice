18ab19883ef4123322ef2a41d2ae856e248c6a79
dailingx <468551414@qq.com>
2025-07-30 17:10:45 +0800

    支持zeroshot

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ from pathlib import Path
 proxy_project_path = Path("/home/workspace/music-content-ai-generate-proxy")
 sys.path.append(str(proxy_project_path))
 from services.status_callback import task_callback
-from services.status_callback import upload_local_file_to_nos
+from services.status_callback import upload_local_file_to_nos, download_file_from_nos
 
 
 # 创建FastAPI应用实例
@@ -69,14 +69,14 @@ async def vllm_tts(request: Request):
     if results:
         all_audio = torch.cat([j['tts_speech'] for j in results], dim=-1)
         output_dir = os.path.join(os.getcwd(), "output")
-        filename = f"sft_instruct_{str(uuid.uuid4()).replace('-', '')}.wav"
+        filename = f"sft_instruct_{spk_id}_{str(uuid.uuid4()).replace('-', '')}.wav"
         file_path = os.path.join(output_dir, filename)
         torchaudio.save(file_path, all_audio, cosyvoice.sample_rate)
         file_abs_path = os.path.abspath(file_path)
         # 获取端口号
         port = app.state.port if hasattr(app.state, 'port') else None
         execution_time = (time.time() - start_time) * 1000
-        logging.info(f"vllm tts success, task_id: {task_id}, tts text: {tts_text}, execution time: {execution_time}")
+        logging.info(f"vllm sft instruct success, task_id: {task_id}, tts text: {tts_text}, execution time: {execution_time}")
         if is_sync:
             upload_result = upload_local_file_to_nos(file_abs_path)
             return {
@@ -90,6 +90,62 @@ async def vllm_tts(request: Request):
             return {
                 "status": "success",
             }
+
+
+@app.post("/vllm/zero/shot")
+async def vllm_zero_shot(request: Request):
+    start_time = time.time()
+    data = await request.json()
+    tts_text = data['text']
+    spk_id = data['speakerId']
+    spk_speech_nos = data['speakerSpeechNosKey']
+    spk_text = data['speakerText']
+    task_id = data['taskId']
+    is_sync = data['isSync']
+    # 判断spk_id是否在spk2info中
+    if spk_id in cosyvoice.frontend.spk2info:
+        results = list(cosyvoice.inference_zero_shot(tts_text, '', '', zero_shot_spk_id=spk_id, stream=False))
+    else:
+        logging.info(f"spk_id {spk_id} 未缓存，将开启缓存")
+        spk_prompt_speech_filename = spk_speech_nos.split('/')[-1]
+        if '.' not in spk_prompt_speech_filename:
+            spk_prompt_speech_filename += '.wav'
+        spk_prompt_speech_path = os.path.join('./asset', f"{spk_id}_{spk_prompt_speech_filename}")
+        download_success = download_file_from_nos(nos_key=spk_speech_nos, save_path=spk_prompt_speech_path)
+        if download_success is not True:
+            return {
+                "status": "fail",
+                "message": "download audio file from nos fail!"
+            }
+        spk_prompt_speech_16k = load_wav(spk_prompt_speech_path, 16000)
+        cosyvoice.add_zero_shot_spk(spk_text, spk_prompt_speech_16k, spk_id)
+        results = list(cosyvoice.inference_zero_shot(tts_text, '', '', zero_shot_spk_id=spk_id, stream=False))
+        cosyvoice.save_spkinfo()
+    if results:
+        all_audio = torch.cat([j['tts_speech'] for j in results], dim=-1)
+        output_dir = os.path.join(os.getcwd(), "output")
+        filename = f"zero_shot_{spk_id}_{str(uuid.uuid4()).replace('-', '')}.wav"
+        file_path = os.path.join(output_dir, filename)
+        torchaudio.save(file_path, all_audio, cosyvoice.sample_rate)
+        file_abs_path = os.path.abspath(file_path)
+        # 获取端口号
+        port = app.state.port if hasattr(app.state, 'port') else None
+        execution_time = (time.time() - start_time) * 1000
+        logging.info(f"vllm zero shot success, task_id: {task_id}, tts text: {tts_text}, execution time: {execution_time}")
+        if is_sync:
+            upload_result = upload_local_file_to_nos(file_abs_path)
+            return {
+                "status": "success",
+                "fileNos": upload_result['fileNos'],
+                "duration": upload_result['duration'],
+                "executionTime": execution_time
+            }
+        else:
+            async_task_callback(task_id, True, file_abs_path, 0, port)
+            return {
+                "status": "success",
+            }
+
 
 # 主函数，用来创建应用实例并运行
 if __name__ == "__main__":
