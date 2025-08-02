f5d17460fbca0ceca7754be0191e7ac893bd3dcf
dailingx <468551414@qq.com>
2025-08-02 14:19:20 +0800

    sft支持传入spk和nos

--- cosyvoice/cli/cosyvoice.py
+++ cosyvoice/cli/cosyvoice.py
@@ -97,14 +97,11 @@ class CosyVoice:
             model_input = self.frontend.frontend_zero_shot(i, prompt_text, prompt_speech_16k, self.sample_rate, zero_shot_spk_id)
             start_time = time.time()
             logging.info('synthesis text {}'.format(i))
-            results = []
             for model_output in self.model.tts(**model_input, stream=stream, speed=speed):
                 speech_len = model_output['tts_speech'].shape[1] / self.sample_rate
-                logging.info('synthesis speech len {}, rtf {}, model_output: {}'.format(speech_len, (
-                            time.time() - start_time) / speech_len, model_output))
-                results.append(model_output)
+                logging.info('yield speech len {}, rtf {}'.format(speech_len, (time.time() - start_time) / speech_len))
+                yield model_output
                 start_time = time.time()
-        return "ok"
 
     def inference_cross_lingual(self, tts_text, prompt_speech_16k, zero_shot_spk_id='', stream=False, speed=1.0, text_frontend=True):
         for i in tqdm(self.frontend.text_normalize(tts_text, split=True, text_frontend=text_frontend)):
