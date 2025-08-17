0b9f4121705a9673e23cd5ae642d4e09ef57e4ec
dailingx <468551414@qq.com>
2025-08-17 19:20:38 +0800

    修改vllm推理引擎参数

--- cosyvoice/cli/model.py
+++ cosyvoice/cli/model.py
@@ -276,7 +276,7 @@ class CosyVoice2Model(CosyVoiceModel):
         from vllm import EngineArgs, LLMEngine
         engine_args = EngineArgs(model=model_dir,
                                  skip_tokenizer_init=True,
-                                 enable_prompt_embeds=False,
+                                 enable_prompt_embeds=True,
                                  gpu_memory_utilization=0.5)
         self.llm.vllm = LLMEngine.from_engine_args(engine_args)
         self.llm.lock = threading.Lock()
